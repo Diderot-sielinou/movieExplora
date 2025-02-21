0e04ccbe0b57421bd8ad5b7e6ccbb44b6a71e424
diderot-sielinou <diderotsielinou@gmail.com>
2025-02-21 22:00:10 +0100

    refactorin my code

--- src/components/LastTrening.jsx
+++ src/components/LastTrening.jsx
@@ -1,44 +1,41 @@
 import NumberCartSilider from "./NumberCartSilider";
-import { useEffect, useState } from "react";
-import { getMovie } from "../api";
+import { useEffect } from "react";
+// import { getMovie } from "../api";
 import useFetch from "../hooks/useFetch";
 
 export default function LastTrening() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setEror] = useState(null);
-  // const { treding, setTreding } = useContext(DataContext);
+
   useEffect(() => {
-    handleget();
+    // handleget();
   }, []);
 
-  const { datas } = useFetch(
+  const { datas, loading, error } = useFetch(
     `${import.meta.env.VITE_BASE_URL_TRENDING}api_key=${
       import.meta.env.VITE_API_KEY
     }`
   );
-  console.log("data of personal hooks",datas)
+  
 
-  const handleget = () => {
-    try {
-      getMovie(
-        `${import.meta.env.VITE_BASE_URL_TRENDING}api_key=${
-          import.meta.env.VITE_API_KEY
-        }`
-      ).then((data) => {
-        if (data) {
-          const newData = data.map((item) => {
-            return { ...item, favorie: false };
-          });
-          // setTreding(newData);
-          setData(newData.slice(0,10));
-          setLoading(false);
-        }
-      });
-    } catch (Error) {
-      setEror(Error.message);
-    }
-  };
+  // const handleget = () => {
+  //   try {
+  //     getMovie(
+  //       `${import.meta.env.VITE_BASE_URL_TRENDING}api_key=${
+  //         import.meta.env.VITE_API_KEY
+  //       }`
+  //     ).then((data) => {
+  //       if (data) {
+  //         const newData = data.map((item) => {
+  //           return { ...item, favorie: false };
+  //         });
+  //         // setTreding(newData);
+  //         setData(newData.slice(0, 10));
+  //         setLoading(false);
+  //       }
+  //     });
+  //   } catch (Error) {
+  //     setEror(Error.message);
+  //   }
+  // };
 
   if (loading) {
     return (
@@ -55,7 +52,7 @@ export default function LastTrening() {
   }
   return (
     <div>
-      <NumberCartSilider treding={data} />
+      <NumberCartSilider treding={datas.slice(0,10)} />
     </div>
   );
 }
--- src/components/NumberCartSilider.jsx
+++ src/components/NumberCartSilider.jsx
@@ -1,26 +1,24 @@
 import PropTypes from "prop-types";
-// import {useContext} from 'react'
-// import { DataContext } from "../context/context";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { motion } from "framer-motion";
 // import Swiper core and required modules
-import {
-  Scrollbar,
-  A11y,
-  Autoplay,
-} from "swiper/modules";
+import { Scrollbar, A11y, Autoplay } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
 
 import "swiper/css/scrollbar";
 import { imgPatch } from "../constant";
+import { useNavigate } from "react-router-dom";
 
 export default function NumberCartSilider({ treding }) {
-  // const  {treding} = useContext(DataContext)
-  // console.log(treding)
+  const navigate = useNavigate();
+  const handleNavigation = (movie) => {
+    navigate(`/detail/${movie.id}`, { replace: true });
+  };
+
   return (
     <div className=" w-full  text-white pl-12 h-auto">
       <motion.div
@@ -45,28 +43,29 @@ export default function NumberCartSilider({ treding }) {
               slidesPerView: 2,
             },
             1024: {
-              slidesPerView: 7.5,
+              slidesPerView: 5,
             },
           }}
           loop={true}
-          modules={[ Scrollbar, A11y, Autoplay]}
-          spaceBetween={0}
-          slidesPerView="auto"
+          modules={[Scrollbar, A11y, Autoplay]}
+          spaceBetween={80}
+          slidesPerView={5}
           scrollbar={{ draggable: true }}
           centeredSlides={false}
           autoplay={{
-            delay: 3500,
+            delay: 4500,
             disableOnInteraction: true,
           }}
           className=" w-full flex items-center mb-10 md:pl-12 h-full"
         >
           {treding?.map((movie, index) => (
             <SwiperSlide key={movie.id}>
-              <div className=" relative text-right w-auto h-[250px] flex items-center  ">
+              <div className=" relative text-right w-auto h-[250px] flex items-center cursor-pointer  ">
                 <span className="font-Roboto text-[250px] font-[1000] z-1 ">
                   {index + 1}
                 </span>
                 <img
+                  onClick={()=> handleNavigation(movie)}
                   className="w-[136px] h-[178px] object-cover object-center border-1 border-white/70 rounded-[12px] absolute z-2 top-[46px] left-[80px] "
                   src={`${imgPatch}${movie.poster_path}`}
                   alt={movie.name}
